38c65bdfe0c5b20e69a8f867e161f5b125410c8e
ZeynepA <zeyneppaytop@gmail.com>
2023-11-16 21:50:26 +0300

    CucumberLessonsProject 16.11.2023

--- src/test/java/stepdefinitions/AmazonSD.java
+++ src/test/java/stepdefinitions/AmazonSD.java
@@ -15,6 +15,7 @@ public class AmazonSD {
     public void kullanici_amazon_sayfasina_gider() {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
     }
+
     @When("kullanici {string} icin arama yapar")
     public void kullanici_icin_arama_yapar(String string) {
         ap.searchKeyword(string);
@@ -24,6 +25,7 @@ public class AmazonSD {
     public void kullanici_title_da_gormelidir(String string) {
         ap.validateResultText(string);
     }
+
     @Given("kullanici arama sonuclarinin {string} icerdigini test eder")
     public void kullanici_arama_sonuclarinin_icerdigini_test_eder(String string) {
        ap.validateResultText(string);
